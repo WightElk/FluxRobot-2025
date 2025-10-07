f5c7505df3b6bf28da83cefa68bc7973e73f17bd
Victor <vic.gorlov@gmail.com>
2025-10-07 15:21:59 -0400

    Add Elevator control

--- src/main/java/frc/robot/CoralRobotContainer.java
+++ src/main/java/frc/robot/CoralRobotContainer.java
@@ -2,10 +2,16 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.TraySubsystem;
+
 /**
  * Robot with Coral Elevator
  */
 public class CoralRobotContainer extends RobotContainer {
+  private final ElevatorSubsystem elevator = new ElevatorSubsystem();
+  private final TraySubsystem tray = new TraySubsystem();
 
   public CoralRobotContainer() {
     super();
@@ -14,5 +20,14 @@ public class CoralRobotContainer extends RobotContainer {
   @Override
   protected void configureBindings() {
     super.configureBindings();
+
+    //Elevator bindings
+    driverController.povDown().whileTrue(new RunCommand(() -> elevator.jogUp(), elevator));
+    driverController.povUp().whileTrue(new RunCommand(() -> elevator.jogDown(), elevator));
+    driverController.povLeft().or(driverController.povRight()).whileTrue(new RunCommand(() -> elevator.stop(), elevator));
+
+    driverController.y().onTrue(new RunCommand(() -> elevator.moveToLevel2(), elevator));
+    driverController.b().onTrue(new RunCommand(() -> elevator.moveToLevel1(), elevator));
+    driverController.a().onTrue(new RunCommand(() -> elevator.moveToBottom(), elevator));
   }
 }
