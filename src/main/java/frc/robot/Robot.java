03910c6e4f6480704e2807e4246ca1af83f94874
Victor <vic.gorlov@gmail.com>
2025-10-11 00:16:31 -0400

    Show robot type on dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.TimedRobot;  
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot {
   public Robot() {
     // RobotController comments are set in RoboRIO Web UI
     String comments = RobotController.getComments();
+    SmartDashboard.putString("Robot", comments);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = comments == "AlgaeRobot" ? new AlgaeRobotContainer() : new CoralRobotContainer();
