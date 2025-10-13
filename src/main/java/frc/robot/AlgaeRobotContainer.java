cb6dd7396acd73cf8dfab14e056e5a352fbfaf6b
vg <vic.gorlov@gmail.com>
2025-10-13 10:28:12 -0400

    Use intake roller in algae robot

--- src/main/java/frc/robot/AlgaeRobotContainer.java
+++ src/main/java/frc/robot/AlgaeRobotContainer.java
@@ -1,14 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.AlgieInCommand;
+import frc.robot.commands.AlgieOutCommand;
 import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.RollerSubsystem;
 
 /**
  * Robot with Algae Intake
  */
 public class AlgaeRobotContainer extends RobotContainer {
-    private final Pneumatics pneumatics = new Pneumatics();
+  private final Pneumatics pneumatics = new Pneumatics();
+  public final RollerSubsystem roller = new RollerSubsystem();
+
+  private final CommandXboxController operatorController =
+    new CommandXboxController(OperatorConstants.OperatorControllerPort);
 
   AlgaeRobotContainer() {
     super(RobotConfig.AlgaeRobot, false);
@@ -20,10 +29,35 @@ public class AlgaeRobotContainer extends RobotContainer {
   protected void configureBindings() {
     super.configureBindings();
 
+    useTwoControllers = SmartDashboard.getBoolean("Use 2 controllers", OperatorConstants.UseTwoControllers);
+
+    CommandXboxController controller = useTwoControllers ? operatorController : driverController;
+
     // Pneumatics bindings
-    driverController.a().onTrue(new InstantCommand(() -> pneumatics.setForward()));
-    driverController.b().onTrue(new InstantCommand(() -> pneumatics.setReverse()));
-    driverController.x().onTrue(new InstantCommand(() -> pneumatics.setOff()));
-    driverController.start().onTrue(new InstantCommand(() -> pneumatics.toggleCompressor()));
+    if (useTwoControllers) {
+    controller.a().onTrue(new InstantCommand(() -> pneumatics.setForward()));
+    controller.b().onTrue(new InstantCommand(() -> pneumatics.setReverse()));
+    controller.y().onTrue(new InstantCommand(() -> pneumatics.setOff()));
+//    controller.start().onTrue(new InstantCommand(() -> pneumatics.toggleCompressor()));
+    controller.start().onTrue(new InstantCommand(() -> pneumatics.enableCompressor()));
+    controller.back().onTrue(new InstantCommand(() -> pneumatics.disableCompressor()));
+
+    controller.leftBumper().whileTrue(new AlgieInCommand(roller));
+      
+    // Here we use a trigger as a button when it is pushed past a certain threshold
+    controller.leftTrigger(OperatorConstants.TriggerThreshold).whileTrue(new AlgieOutCommand(roller));
+  }
+    else {
+      controller.a().onTrue(new InstantCommand(() -> pneumatics.setForward()));
+      controller.b().onTrue(new InstantCommand(() -> pneumatics.setReverse()));
+      controller.y().onTrue(new InstantCommand(() -> pneumatics.setOff()));
+      controller.start().onTrue(new InstantCommand(() -> pneumatics.enableCompressor()));
+      controller.back().onTrue(new InstantCommand(() -> pneumatics.disableCompressor()));
+  
+      controller.leftBumper().whileTrue(new AlgieOutCommand(roller));
+      
+      // Here we use a trigger as a button when it is pushed past a certain threshold
+      controller.leftTrigger(OperatorConstants.TriggerThreshold).whileTrue(new AlgieInCommand(roller));
+    }
   }
 }
