e736d5dd1d1b4cf2ce2abde4eb9702638a8efc86
vg <victor.gorlov@gmail.com>
2025-10-19 13:27:13 -0400

    Compare robot name

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putString("Robot", comments);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = comments == "AlgaeRobot" ? new AlgaeRobotContainer() : new CoralRobotContainer();
+    m_robotContainer = comments.contains("AlgaeRobot") ? new AlgaeRobotContainer() : new CoralRobotContainer();
   }
 
   /**
